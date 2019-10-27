f5eef4f811674f3f30300f25b938ba8a8ba6ec68
irwantowibowo1992 <irwantowibowo1992@hotmail.com>
2019-10-27 10:28:38 -0700

    membuat layout

--- App.js
+++ App.js
@@ -11,36 +11,42 @@ import styles from './style/Style'
 
 class App extends Component {
 
-  constructor() {
-    super()
+  // constructor() {
+  //   super()
 
-    this.state = {
-      jumlah: 0
-    }
-  }
+  //   this.state = {
+  //     jumlah: 0
+  //   }
+  // }
 
-  handleTambah = () => {
-    this.setState({
-      jumlah: this.state.jumlah + 1
-    })
-  }
+  // handleTambah = () => {
+  //   this.setState({
+  //     jumlah: this.state.jumlah + 1
+  //   })
+  // }
 
   render(){
     return(
-      <View style = {styles.container}>
-        <StatusBar backgroundColor = "blue" />
+      // <View style = {styles.container}>
+      //   <StatusBar backgroundColor = "blue" />
 
-        {/* panggil class Header */}
-        {/* <Header person = "Name : Alex" /> */}
-        <Text style={styles.header}>Hello React Native</Text>
-        <Header old = "Age : 25" />
+      //   {/* panggil class Header */}
+      //   {/* <Header person = "Name : Alex" /> */}
+      //   <Text style={styles.header}>Hello React Native</Text>
+      //   <Header old = "Age : 25" />
 
-        <Footer tahun = "2019" />
+      //   <Footer tahun = "2019" />
 
-        <Button title="Button Tambah" onPress={this.handleTambah} />
+      //   <Button title="Button Tambah" onPress={this.handleTambah} />
 
-        <Text style={styles.jumlah}>Jumlah : {this.state.jumlah}</Text>
+      //   <Text style={styles.jumlah}>Jumlah : {this.state.jumlah}</Text>
+      // </View>
+
+      <View style={{ flex: 1, backgroundColor: 'red' }}>
+        <Text style={{ flex: 1, backgroundColor: 'yellow', margin: 20, borderRadius: 25 }}>Halaman 1</Text>
+        <Text style={{ flex: 2, backgroundColor: 'green' }}>Halaman 2</Text>
       </View>
+
     )
   }
 }
